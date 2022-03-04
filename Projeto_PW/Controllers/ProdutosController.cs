dc3ec50b5a788091459ed80ad4c619f13e8c9a01
Angelo Ronchi016 <Angeo.ronchi@gmail.com>
2022-03-04 14:45:56 -0300

    Adição do create e do index do produt

--- Projeto_PW/Controllers/ProdutosController.cs
+++ Projeto_PW/Controllers/ProdutosController.cs
@@ -9,12 +9,16 @@ namespace Projeto_PW.Controllers
         public static List<ProdutoModel> lsProdutos = new List<ProdutoModel>();
         public IActionResult Index()
         {
+            ProdutoModel produto = new ProdutoModel();
+            produto.Descricao = "Mouse Gamer";
+            produto.Codigo = 2538;
+            lsProdutos.Add(produto);
             return View(lsProdutos);
         }
 
         public IActionResult Create()
         {
-            return View();
+            return View(lsProdutos);
         }
 
         [HttpPost]
